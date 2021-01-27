26f8378400484d8cbb15237139d26cf6331ff419
mohamedsugroo <mohamedsugroo@gmail.com>
2021-01-27 21:45:49 +0000

    request format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::API
 
+    before_filter :default_request_format
+
+    def default_request_format
+    request.format = :json
+    end
+
+
     # render error massage
     def render_error_msg msg = 'error'
         render json: {
