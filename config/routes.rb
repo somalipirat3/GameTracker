915dca8d4526a5dab9ce16549d6efc1e23ba3b4b
mohamedsugroo <mohamedsugroo@gmail.com>
2021-01-27 22:18:37 +0000

    db auth

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
       get '/apexlegends/profiles/:platform/:username', to: 'profiles#show'
       get '/apexlegends/profiles/search', to: 'profiles#search' # params platform and username
       # get '/apexlegends/profiles/search/:platform/:username', to: 'profiles#search'
-
-
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
